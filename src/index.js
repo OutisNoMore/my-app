bafdd1cadb92d383da5af49ea60c330af94f90b2
OutisNoMore <OutisNoMore@protonmail.com>
2022-01-13 07:45:54 -0800

    finish 2 player tic tac toe

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+/*
 class Square extends React.Component {
   constructor(props){
     super(props);
@@ -35,24 +35,120 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()})}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
+*/
+
+let players = ['X', 'O'];
+let status = '';
+let turn = 0;
+
+function Square(props){
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      over: false,
+//      turn: 0,
+//      status: '',
+    };
+  }
+
+  checkRow(squares, i){
+    let row = Math.floor(i/3);
+
+    return squares[3*row] === squares[3*row+1] && squares[3*row] === squares[3*row+2];
+  }
+
+  checkColumn(squares, i){
+    let col = i % 3;
+
+    return squares[col] === squares[col+3] && squares[col] === squares[col+6];
+  }
+
+  checkDiagonal(squares, i){
+    if(i !== 4 && i !== 6 && i !== 2){
+      return false;
+    }
+
+    return squares[4] === squares[2] && squares[4] === squares[6];
+  }
+
+  checkAnti(squares, i){
+    if(i !== 4 && i !== 0 && i !== 8){
+      return false;
+    }
+
+    return squares[4] === squares[0] && squares[4] === squares[8];
+  }
+
+  isWinner(squares, i){
+//    row = Math.floor(i/3);
+//    col = i % 3;
+    if(this.checkRow(squares, i)      || 
+       this.checkColumn(squares, i)   || 
+       this.checkDiagonal(squares, i) ||
+       this.checkAnti(squares, i)){
+      return true;
+    }
+
+    return false;
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+//    squares[i] = players[this.state.turn];
+    squares[i] = players[turn];
+    this.setState({squares: squares});
+    
+    if(this.isWinner(squares, i)){
+      // this.setState({status: 'Winner: ' + players[this.state.turn]});
+      status = 'Winner: ' + players[turn];
+      this.setState({over: true});
+    }
+    else{
+      turn === 1 ? turn = 0 : turn++;
+    }
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => {
+          if(!this.state.over && !this.state.squares[i]){
+            this.handleClick(i);
+          }
+        }}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+//    this.setState({status: 'Current player: ' + players[this.state.turn]});
+    if(!this.state.over){
+      status = 'Current player: ' + players[turn];
+    }
 
     return (
       <div>
-        <div className="status">{status}</div>
+//        <div className="status">{this.state.status}</div>
+      <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
