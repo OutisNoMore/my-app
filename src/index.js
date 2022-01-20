cb05d9aaa02b17996ccd47455f48a6fdeefb6880
OutisNoMore <OutisNoMore@protonmail.com>
2022-01-20 09:28:21 -0800

    Check for draw

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -47,7 +27,7 @@ class Square extends React.Component {
 */
 
 let players = ['X', 'O'];
-let status = '';
+let status = 'Current Player: X';
 let turn = 0;
 
 function Square(props){
@@ -63,9 +43,8 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      over: false,
-//      turn: 0,
-//      status: '',
+      gameOver: false,
+      numbTurns: 0,
     };
   }
 
@@ -98,8 +77,6 @@ class Board extends React.Component {
   }
 
   isWinner(squares, i){
-//    row = Math.floor(i/3);
-//    col = i % 3;
     if(this.checkRow(squares, i)      || 
        this.checkColumn(squares, i)   || 
        this.checkDiagonal(squares, i) ||
@@ -112,17 +89,21 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
-//    squares[i] = players[this.state.turn];
     squares[i] = players[turn];
     this.setState({squares: squares});
+    this.setState({numbTurns: this.state.numbTurns + 1});
     
     if(this.isWinner(squares, i)){
-      // this.setState({status: 'Winner: ' + players[this.state.turn]});
       status = 'Winner: ' + players[turn];
-      this.setState({over: true});
+      this.setState({gameOver: true});
+    }
+    else if(this.state.numbTurns === 8){
+      status = 'Draw: No winners';
+      this.setState({gameOver: true});
     }
     else{
       turn === 1 ? turn = 0 : turn++;
+      status = "Current Player: " + players[turn];
     }
   }
 
@@ -131,7 +112,7 @@ class Board extends React.Component {
       <Square 
         value={this.state.squares[i]} 
         onClick={() => {
-          if(!this.state.over && !this.state.squares[i]){
+          if(!this.state.gameOver && !this.state.squares[i]){
             this.handleClick(i);
           }
         }}
@@ -140,14 +121,14 @@ class Board extends React.Component {
   }
 
   render() {
-//    this.setState({status: 'Current player: ' + players[this.state.turn]});
-    if(!this.state.over){
-      status = 'Current player: ' + players[turn];
+    if(this.state.gameOver){
+      if(window.confirm(status + "\nStart a new game?")){
+        window.location.reload();
+      }
     }
 
     return (
       <div>
-//        <div className="status">{this.state.status}</div>
       <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
